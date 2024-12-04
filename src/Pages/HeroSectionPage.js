f2b7f045a3b113edab7c0fb8a46b90efeaec24ff
Doron <doronbrgr@yahoo.com>
2024-12-04 18:23:23 +0200

    hero page

---
+++ src/Pages/HeroSectionPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HeroSectionPage = () => {
+  return (
+    <div>
+      <h1>Does Your Broker Offer You All This?</h1>
+    </div>
+  );
+};
+
+export default HeroSectionPage;
